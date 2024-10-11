81425e6f8adc7d621e17a195cc476ce566fdfbbc
obada kraishan <obada.kraishan@gmail.com>
2024-10-11 04:56:12 -0500

    Add a Seeder

---
+++ backend/seeder.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const doctors = require('./data/doctors');  // Sample doctor data
+const Doctor = require('./models/Doctor');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const importData = async () => {
+  try {
+    await Doctor.deleteMany();
+    await Doctor.insertMany(doctors);
+    console.log('Doctors imported!');
+    process.exit();
+  } catch (error) {
+    console.error('Error with data import:', error);
+    process.exit(1);
+  }
+};
+
+importData();
