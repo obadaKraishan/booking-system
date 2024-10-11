509fb3d5ad96b4c3ca50597ad978d7059ebaeeee
obada kraishan <obada.kraishan@gmail.com>
2024-10-10 20:20:12 -0500

    Add Backend Port

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,5 @@ app.use(express.json());
 // Routes
 app.use('/api/appointments', appointmentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
