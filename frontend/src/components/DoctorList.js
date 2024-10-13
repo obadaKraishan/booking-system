80f3244acd74fb7b5e5440e574c6d49c19c9ba91
obada kraishan <obada.kraishan@gmail.com>
2024-10-12 19:17:02 -0500

    Add Doctor List

---
+++ frontend/src/components/DoctorList.js
@@ -0,0 +1,34 @@
+// src/components/DoctorList.js
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import DoctorCard from './DoctorCard';
+
+const DoctorList = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:5001/api/doctors'); // Replace with your backend URL
+        setDoctors(data);
+      } catch (error) {
+        console.error('Error fetching doctors:', error);
+      }
+    };
+
+    fetchDoctors();
+  }, []);
+
+  return (
+    <div className="container mx-auto">
+      {doctors.length > 0 ? (
+        doctors.map((doctor) => <DoctorCard key={doctor._id} doctor={doctor} />)
+      ) : (
+        <p className="text-gray-700">No doctors available.</p>
+      )}
+    </div>
+  );
+};
+
+export default DoctorList;
