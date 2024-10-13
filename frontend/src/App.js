bf1c72975fc32c307a44d42f637faf9de6920796
obada kraishan <obada.kraishan@gmail.com>
2024-10-12 20:05:36 -0500

    Add Doctor Details Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+// src/App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
+import DoctorDetails from './pages/DoctorDetails'; // Import the new DoctorDetails page
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,6 +19,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/doctor/:id" element={<DoctorDetails />} /> {/* Doctor Details Route */}
         </Routes>
       </Router>
       <ToastContainer />
